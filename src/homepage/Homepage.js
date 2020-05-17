0c1a1cf3bc4005710f6c8f4434a7f8213d4cdcde
Jina Zhu <jinazhu87@gmail.com>
2020-05-17 12:28:16 -0700

    added font awesome and apply icons to buttons

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Homepage = () => {
   return (
     <div>
       <form>
         <input type="text" placeholder="enter ingredient"></input>
-        <button type="submit"></button>
+        <button type="submit">
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+        <button type="submit">
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
       </form>
     </div>
   );
