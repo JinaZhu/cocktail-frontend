cfb43177107ed562b168cc6f7e8b9fa165eb2742
Jina Zhu <jinazhu87@gmail.com>
2020-05-17 12:22:07 -0700

    setup routes on app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import './App.css';
-import Register from './userInfo/register';
-import Login from './userInfo/login';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Register from "./userInfo/register";
+import Login from "./userInfo/login";
+import Homepage from "./homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
-      <Register />
-      <br></br>
-      <Login />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="enter ingredient"></input>
+        <button type="submit"></button>
+      </form>
+    </div>
+  );
+};
+
+export default Homepage;
