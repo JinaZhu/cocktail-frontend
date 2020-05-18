c6cff924ce5a49d13fe32397abf3b686ff7b4b5c
Jina Zhu <jinazhu87@gmail.com>
2020-05-17 17:25:05 -0700

    style list item

---
+++ src/homepage/HomepageStyles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const IngredientDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const IngredientUl = styled.ul`
+  min-width: 25%;
+  list-style: none;
+`;
+export const IngredientContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  min-width: 30%;
+  margin: 0.5rem;
+  background: #f1ece1;
+`;
+
+export const IngredientItem = styled.li`
+  margin: 0 auto;
+`;
+
+export const DeleteIngredientButton = styled.button`
+  color: #f1ece1;
+  border: none;
+  padding: 0.5rem;
+  cursor: pointer;
+  font-size: 1rem;
+`;
--- src/homepage/IngredientList.js
+++ src/homepage/IngredientList.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  IngredientContainer,
+  IngredientItem,
+  IngredientDiv,
+  IngredientUl,
+  DeleteIngredientButton,
+} from "./HomepageStyles";
 
 const IngredientList = (ingredients) => {
   const allIngredients = ingredients.ingredients.map((ingredient, index) => {
-    return <li key={index}>{ingredient}</li>;
+    return (
+      <IngredientContainer key={index}>
+        <IngredientItem>{ingredient}</IngredientItem>
+        <DeleteIngredientButton>
+          <FontAwesomeIcon icon={faTimes} />
+        </DeleteIngredientButton>
+      </IngredientContainer>
+    );
   });
 
   return (
-    <div>
-      <ul>{allIngredients}</ul>
-    </div>
+    <IngredientDiv>
+      <IngredientUl>{allIngredients}</IngredientUl>
+    </IngredientDiv>
   );
 };
 
