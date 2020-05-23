700a7d10e82fd049a3822b0604e74b488b7c4b3a
Jina Zhu <jinazhu87@gmail.com>
2020-05-23 11:47:46 -0700

    added div to hold ingredients as a list

--- src/homepage/results/Results.js
+++ src/homepage/results/Results.js
@@ -2,12 +2,18 @@ import React from "React";
 
 const Results = ({ results }) => {
   const allResults = results.map((result, index) => {
-    <div>
+    <div key={index}>
       <img src={result.image} alt="cocktail image" width="250" />
       <div>
         <h2>{result.name}</h2>
       </div>
-      <div></div>
+      <div>
+        <ul>
+          {result.ingredients.map((ingredient, index) => {
+            return <li key={index}>{ingredient}</li>;
+          })}
+        </ul>
+      </div>
     </div>;
   });
 
