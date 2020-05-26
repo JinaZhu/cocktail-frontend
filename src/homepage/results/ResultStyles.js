f9b01d44251082e8939038ccbb5a25ad86e81a8a
Jina Zhu <jinazhu87@gmail.com>
2020-05-25 21:25:10 -0700

    display results on homepage

--- src/homepage/results/ResultStyles.js
+++ src/homepage/results/ResultStyles.js
@@ -1,16 +1,25 @@
 import styled from "styled-components";
 
-export const ResultContainer = styled.div`
+export const CocktailsContainer = styled.div`
   display: flex;
-  flex-direction: column;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
+  margin: 50px;
+`;
+
+export const ResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+  border: 1px solid black;
 `;
 
 export const CocktailImage = styled.img``;
 
 export const CocktailTitleContainer = styled.div`
-  border: 2px solid black;
+  border: 1px solid black;
   margin: 10px;
 `;
--- src/homepage/results/Results.js
+++ src/homepage/results/Results.js
@@ -1,8 +1,12 @@
-import React from "React";
+import React from "react";
 
 import cherryCocktail from "../../static/cherry-cocktail.jpg";
 
-import { ResultContainer, CocktailTitleContainer } from "./ResultStyles";
+import {
+  ResultContainer,
+  CocktailTitleContainer,
+  CocktailsContainer,
+} from "./ResultStyles";
 
 const Results = () => {
   const results = [
@@ -24,20 +28,30 @@ const Results = () => {
   ];
 
   const allResults = results.map((result, index) => {
-    <ResultContainer key={index}>
-      <img src={result.image} alt="cocktail image" width="250" />
-      <CocktailTitleContainer>
-        <h2>{result.name}</h2>
-      </CocktailTitleContainer>
-      <div>
-        <ul>
-          {result.ingredients.map((ingredient, index) => {
-            return <li key={index}>{ingredient}</li>;
-          })}
-        </ul>
-      </div>
-    </ResultContainer>;
+    return (
+      <ResultContainer key={index}>
+        <img
+          src={result.image}
+          alt="cocktail image"
+          width="250"
+          height="250"
+          object-fit="cover"
+        />
+        <CocktailTitleContainer>
+          <h2>{result.name}</h2>
+        </CocktailTitleContainer>
+        <div>
+          <ul>
+            {result.ingredients.map((ingredient, index) => {
+              return <li key={index}>{ingredient}</li>;
+            })}
+          </ul>
+        </div>
+      </ResultContainer>
+    );
   });
 
-  return <div>{allResults}</div>;
+  return <CocktailsContainer>{allResults}</CocktailsContainer>;
 };
+
+export default Results;
