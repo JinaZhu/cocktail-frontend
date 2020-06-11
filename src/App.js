2aa975c367e2b2509ac48ee2c44d78a1dc7c8c7d
Kimberly Chuc <kimmchuc@gmail.com>
2020-06-11 13:01:44 -0700

    added onSubmit handler

--- src/App.js
+++ src/App.js
@@ -2,35 +2,30 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./navbar/Navbar";
+// import Register from "./userInfo/register";
+// import Login from "./userInfo/login";
 import Homepage from "./homepage/Homepage";
-import { Background } from "./homepage/HomepageStyles";
-
 
 function App() {
 
-  const [user, setUser] = useState('');
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [user, setUser] = useState(false);
+  const [value, setValue] = useState('');
 
   // check if user is logged in
-  async function checkLoggedIn() {
-    const userName = await fetch('/getUser')
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userName = await fetch('/getUser');
       try {
         const jsonUserName = await userName.json();
         if (jsonUserName.response) {
-          // console.log(jsonUserName.response)
-          setUser(jsonUserName.response);
+          setUser(true);
       }
     } catch (error) {
       console.log(`Error: ${error}`)
       }
-  }
-
-  useEffect(() => {
-    checkLoggedIn()
-    setTimeout(() => {
-      setIsLoaded(true);
-    }, 100);
-  }, [])
+    }
+    fetchUser();
+  }, []);
 
   // handles user logging out
   const userLogout = (evt) => {
@@ -41,27 +36,40 @@ function App() {
         'Content-Type': 'application/json',
       }
     });
-    setUser('')
+    setUser(false)
     alert('Successfully logged out!')
 
   }
   return (
-    <Background className="App" style={{display: 'grid'}}>
-      {
-        isLoaded && (
-        <>
-          <Navbar user={user} setUser={setUser} userLogout={userLogout}/>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Homepage} />
-              <Route path="/displaySavedCocktails" component={Homepage} />
-            </Switch>
-          </Router>
-        </>
-        ) 
-      }
-    </Background>
+    <div className="App">
+      <Navbar user={user} setUser={setUser} userLogout={userLogout}/>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/displaySavedCocktails" component={Homepage} />
+        </Switch>
+      </Router>
+    </div>
   );
+
+  //sending ingredients to server.py when clicking submit
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    const data = { ingredients: value };
+    console.log('submit');
+    console.log(value);
+    fetch('/ingredientsresults.json', {
+      method: 'POST',
+      headers: {
+
+      }
+    })
+  }
+
+  function handleValue(e) {
+    setValue(e.target.value);
+  }
 }
 
 export default App;
